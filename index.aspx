ee5700693497019b900240dc4d4cb78f1e754b38
Yixian Ma <ma.yixian0619@gmail.com>
2021-10-22 17:26:31 -0400

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,184 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="HondiBuildPrice.ASP.index" %>
+
+<!DOCTYPE html>
+<!-- 
+    Author: Yixian Ma
+    Created at: 2021-10-22
+    -->
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hondi Build & Price</title>
+    <style>
+        .center-text{
+            text-align:center;
+        }
+        .column1-width{
+            width:130px;
+        }
+        .column2-width{
+            width:270px;
+        }
+        .form{
+            height:700px;
+        }
+        .CarInfoHeight{
+            height:120px;
+        }
+        .backColor{
+            background-color:azure;
+        }
+        .panColor{
+            background-color:aquamarine;
+        }
+        .hide{
+            overflow:hidden;
+        }
+        .btn{
+            border:1px solid #563d7c;
+            border-radius:8px;
+            padding: 8px 5px 8px 5px;
+            color: white;
+            background-color:darkblue;
+        }
+    </style>
+</head>
+<body class="backColor">
+    <form id="form1" runat="server" class="form">
+        <div class="center-text">
+            <h1>HONDI BUILD & PRICE</h1>
+            <table align="center">
+                <tr>
+                    <td>
+                        <asp:Image ID="imgLogo" runat="server" Height="100px" Width="100px" ImageUrl="~/logo.png"/>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <hr />
+        <br />
+
+        <table>
+            <tr style="vertical-align:top">
+                <td class="CarInfoHeight">
+                    <asp:Panel ID="panCarInfo" runat="server" GroupingText="Car Information" Height="100%" Width="400px" CssClass="panColor">
+                        <table>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblCity" runat="server" Text="Your City: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:TextBox ID="txtCity" runat="server" Width="100px" CssClass="center-text"></asp:TextBox>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblZipCode" runat="server" Text="Zip Code: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:TextBox ID="txtZipCode" runat="server" Width="100px" CssClass="center-text"></asp:TextBox>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblCarModel" runat="server" Text="Select Car Model: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:DropDownList ID="cboCar" runat="server" Width="200px" AutoPostBack="true" OnSelectedIndexChanged="cboCar_SelectedIndexChanged" CssClass="center-text"></asp:DropDownList>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblInteriorColor" runat="server" Text="Interior Color: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:ListBox ID="lstColor" runat="server" AutoPostBack="true" Width="150px" Rows="3" CssClass="hide"></asp:ListBox>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblAccessories" runat="server" Text="Accessories: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:CheckBoxList ID="chklstAccessories" runat="server" AutoPostBack="true"></asp:CheckBoxList>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblWarranty" runat="server" Text="Warranty: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:RadioButtonList ID="rdlstWarranty" runat="server" AutoPostBack="true"></asp:RadioButtonList>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblContact" runat="server" Text="Dealer Contact you by phone?"></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:CheckBox ID="chkContact" runat="server" AutoPostBack="true" OnCheckedChanged="chkContact_CheckedChanged"/>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+
+                            <tr>
+                                <td class="column1-width">
+                                    <asp:Label ID="lblPhone" runat="server" Text="Phone Number: "></asp:Label>
+                                </td>
+                                <td class="column2-width">
+                                    <asp:TextBox ID="txtPhone" runat="server" Width="100px" CssClass="center-text"></asp:TextBox>
+                                </td>
+                            </tr>
+
+                            <tr> <td>&nbsp;</td> </tr>
+                        </table>
+                    </asp:Panel>
+                </td>
+
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+
+                <td class="CarInfoHeight">
+                    <asp:Panel ID="panPrice" runat="server" GroupingText="Price Resume" Height="285px" Width="400px" CssClass="panColor">
+                        <asp:Literal ID="litPrice" runat="server"></asp:Literal>
+                        <asp:Button ID="btnConclude" runat="server" Text="Conclude" OnClick="btnConclude_Click" CssClass="btn"/>
+                    </asp:Panel>
+                
+                    <p>&nbsp;</p>
+
+                    <asp:Panel ID="panFinalinfo" runat="server" GroupingText="Final Information" Height="305px" Width="400px" CssClass="panColor">
+                        <asp:Literal ID="litFinal" runat="server"></asp:Literal>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </asp:Panel>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+/*
+    Author: Yixian Ma
+    Created at: 2021-10-22
+*/
+
+namespace HondiBuildPrice.ASP
+{
+    public partial class index : System.Web.UI.Page
+    {
+        static DataTable tbCar;
+        static DataTable tbColor;
+        static DataTable tbAccessories;
+        static DataTable tbWarranty;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            panFinalinfo.Visible = panPrice.Visible = false;
+
+            tbCar = CreateCarTable();
+            tbColor = CreateColorTable();
+            tbAccessories = CreateAcessTable();
+            tbWarranty = CreateWarrantyTable();
+
+            if (!Page.IsPostBack)
+            {
+                lblPhone.Visible = txtPhone.Visible = false;
+
+                cboCar.DataTextField = "Info";
+                cboCar.DataValueField = "Price";
+                cboCar.DataSource = tbCar;
+                cboCar.DataBind();
+
+                lstColor.DataTextField = "Info";
+                lstColor.DataValueField = "Price";
+                lstColor.DataSource = tbColor;
+                lstColor.DataBind();
+                lstColor.SelectedIndex = 0;
+
+                chklstAccessories.DataTextField = "Info";
+                chklstAccessories.DataValueField = "Price";
+                chklstAccessories.DataSource = tbAccessories;
+                chklstAccessories.DataBind();
+
+                rdlstWarranty.DataTextField = "Info";
+                rdlstWarranty.DataValueField = "Price";
+                rdlstWarranty.DataSource = tbWarranty;
+                rdlstWarranty.DataBind();
+                rdlstWarranty.SelectedIndex = 0;
+            }
+
+            if (cboCar.SelectedIndex > 0)
+            {
+                calulatePrice();
+            }
+
+        }
+
+        private DataTable CreateCarTable()
+        {
+            DataTable tbCar = new DataTable("Cars");
+
+            tbCar.Columns.Add("Brand", typeof(String));
+            tbCar.Columns.Add("Info", typeof(String));
+            tbCar.Columns.Add("Price", typeof(Int32));
+
+            DataRow myRow = tbCar.NewRow();
+            myRow["Brand"] = "";
+            myRow["Info"] = "Select a Car";
+            myRow["Price"] = 0;
+            tbCar.Rows.Add(myRow); 
+            
+            myRow = tbCar.NewRow();
+            myRow["Brand"] = "Civoc";
+            myRow["Info"] = "Civoc (+ $25000)";
+            myRow["Price"] = 25000;
+            tbCar.Rows.Add(myRow);
+
+            myRow = tbCar.NewRow();
+            myRow["Brand"] = "DR-V";
+            myRow["Info"] = "DR-V (+ $30000)";
+            myRow["Price"] = 30000;
+            tbCar.Rows.Add(myRow);
+
+            myRow = tbCar.NewRow();
+            myRow["Brand"] = "Appord";
+            myRow["Info"] = "Appord (+ $33000)";
+            myRow["Price"] = 33000;
+            tbCar.Rows.Add(myRow);
+
+            myRow = tbCar.NewRow();
+            myRow["Brand"] = "Cilot";
+            myRow["Info"] = "Cilot (+ $45000)";
+            myRow["Price"] = 45000;
+            tbCar.Rows.Add(myRow);
+
+            myRow = tbCar.NewRow();
+            myRow["Brand"] = "Odyrrey";
+            myRow["Info"] = "Odyrrey (+ $54000)";
+            myRow["Price"] = 54000;
+            tbCar.Rows.Add(myRow);
+
+            return tbCar;
+        }
+
+        private DataTable CreateColorTable()
+        {
+            DataTable tbColor = new DataTable("Colors");
+
+            tbColor.Columns.Add("Color", typeof(String));
+            tbColor.Columns.Add("Info", typeof(String));
+            tbColor.Columns.Add("Price", typeof(Int32));
+
+            DataRow myRow = tbColor.NewRow();
+            myRow["Color"] = "White";
+            myRow["Info"] = "White (Free)";
+            myRow["Price"] = 0;
+            tbColor.Rows.Add(myRow);
+
+            myRow = tbColor.NewRow();
+            myRow["Color"] = "Dark";
+            myRow["Info"] = "Dark (+ $300)";
+            myRow["Price"] = 300;
+            tbColor.Rows.Add(myRow);
+
+            myRow = tbColor.NewRow();
+            myRow["Color"] = "Pearl";
+            myRow["Info"] = "Pearl(+ $900)";
+            myRow["Price"] = 900;
+            tbColor.Rows.Add(myRow);
+
+            return tbColor;
+        }
+
+        private DataTable CreateAcessTable()
+        {
+            DataTable tbAcess = new DataTable("Accessories");
+
+            tbAcess.Columns.Add("Name", typeof(String));
+            tbAcess.Columns.Add("Info", typeof(String));
+            tbAcess.Columns.Add("Price", typeof(Int32));
+
+            DataRow myRow = tbAcess.NewRow();
+            myRow["Name"] = "Aero Kit";
+            myRow["Info"] = "Aero Kit (+ $1300)";
+            myRow["Price"] = 1300;
+            tbAcess.Rows.Add(myRow);
+
+            myRow = tbAcess.NewRow();
+            myRow["Name"] = "Film";
+            myRow["Info"] = "Film (+ $600)";
+            myRow["Price"] = 600;
+            tbAcess.Rows.Add(myRow);
+
+            myRow = tbAcess.NewRow();
+            myRow["Name"] = "Cold Weather";
+            myRow["Info"] = "Cold Weather (+ $900)";
+            myRow["Price"] = 900;
+            tbAcess.Rows.Add(myRow);
+
+            myRow = tbAcess.NewRow();
+            myRow["Name"] = "Hondi Emblem";
+            myRow["Info"] = "Hondi Emblem (+ $150)";
+            myRow["Price"] = 150;
+            tbAcess.Rows.Add(myRow);
+
+            myRow = tbAcess.NewRow();
+            myRow["Name"] = "Snow Tire Package";
+            myRow["Info"] = "Snow Tire Package (+ $1800)";
+            myRow["Price"] = 1800;
+            tbAcess.Rows.Add(myRow);
+
+            return tbAcess;
+        }
+
+        private DataTable CreateWarrantyTable()
+        {
+            DataTable tbWarranty = new DataTable("Warranty");
+
+            tbWarranty.Columns.Add("Name", typeof(String));
+            tbWarranty.Columns.Add("Info", typeof(String));
+            tbWarranty.Columns.Add("Price", typeof(Int32));
+
+            DataRow myRow = tbWarranty.NewRow();
+            myRow["Name"] = "3 Years";
+            myRow["Info"] = "3 Years (Included)";
+            myRow["Price"] = 0;
+            tbWarranty.Rows.Add(myRow);
+
+            myRow = tbWarranty.NewRow();
+            myRow["Name"] = "5 Years";
+            myRow["Info"] = "5 Years (+ $1000)";
+            myRow["Price"] = 1000;
+            tbWarranty.Rows.Add(myRow);
+
+            myRow = tbWarranty.NewRow();
+            myRow["Name"] = "7 years";
+            myRow["Info"] = "7 Years (+ $1500)";
+            myRow["Price"] = 1500;
+            tbWarranty.Rows.Add(myRow);
+
+            return tbWarranty;
+        }
+
+        private void calulatePrice()
+        {
+            int carPrice = 0, color = 0, acess = 0, warranty = 0, subtotal = 0, total = 0;
+
+            carPrice = Convert.ToInt32(cboCar.SelectedItem.Value);
+            color = Convert.ToInt32(lstColor.SelectedItem.Value);
+            warranty = Convert.ToInt32(rdlstWarranty.SelectedItem.Value);
+
+            foreach (ListItem item in chklstAccessories.Items) {
+                acess += (item.Selected) ? Convert.ToInt32(item.Value) : 0;
+            }
+
+            subtotal = carPrice + color + warranty + acess;
+            total = Convert.ToInt32(subtotal * 1.15);
+
+            litPrice.Text = "<br>Car Price: $" + carPrice + "<br>Interior Color: $" + color
+                + "<br>Acessories: $" + acess + "<br>Warranty: $" + warranty + "<br><br><br><br>Total without taxes: $"
+                + subtotal + "<br>Total with taxes (15%): $" + total + "<br><br><br>";
+
+            panPrice.Visible = true;
+        }
+
+        protected void cboCar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void chkContact_CheckedChanged(object sender, EventArgs e)
+        {
+            lblPhone.Visible = txtPhone.Visible = chkContact.Checked;
+        }
+
+        protected void btnConclude_Click(object sender, EventArgs e)
+        {
+            panFinalinfo.Visible = true;
+
+            foreach (DataRow myRow in tbCar.Rows) {
+                if (myRow["Info"].ToString() == cboCar.SelectedItem.Text) {
+                    litFinal.Text = "<br>Congratulation in obtain your new Hondi " + myRow["Brand"].ToString() + " in " + txtCity.Text + ", " + txtZipCode.Text + ".<br><br>";
+                }
+            }
+
+            foreach (DataRow myRow in tbColor.Rows) {
+                if (myRow["Info"].ToString() == lstColor.SelectedItem.Text) {
+                    litFinal.Text += "Your car comes with " + myRow["Color"].ToString() + " Interior Color.<br>";
+                }
+            }
+
+            foreach (ListItem item in chklstAccessories.Items) {
+                if (item.Selected) {
+                    foreach (DataRow myRow in tbAccessories.Rows){
+                        if (myRow["Info"].ToString() == item.Text){
+                            litFinal.Text += myRow["Name"].ToString() + ", ";
+                        }
+                    }
+                }  
+            }
+
+            foreach (DataRow myRow in tbWarranty.Rows) {
+                if (myRow["Info"].ToString() == rdlstWarranty.SelectedItem.Text) {
+                    litFinal.Text += "<br><br>You choosed " + myRow["Name"].ToString() + " of warranty";
+                }
+            }
+
+            if (chkContact.Checked)
+            {
+                litFinal.Text += " and our dealer will contact you by the phone number " + txtPhone.Text + ".";
+            }
+        }
+    }
+}
